32f78ca207c99841047494f81bf2f43ea5e9116a
DESKTOP-DCIMHCD\박지훈 <qkrwlgnstm@naver.com>
2017-04-12 08:06:52 +0900

    업로드

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,71 @@
 var gulp = require('gulp'),
+    clean = require('gulp-clean'),
  	fileinclude = require('gulp-file-include'),
  	sass = require('gulp-sass'),
  	htmlhint  = require('gulp-htmlhint'),
-    csslint = require('gulp-csslint'),
-    webreporter = require('gulp-hint-web-reporter'),
+    sassLint = require('gulp-sass-lint'),
     gutil = require('gulp-util'),
     sourcemaps = require('gulp-sourcemaps'),
-	browserSync = require('browser-sync');
+    jslint = require('gulp-jslint'),
+    concat = require('gulp-concat'),
+    clone = require('gulp-clone');
 
 const autoprefixer = require('gulp-autoprefixer');
 
+gulp.task('clean', function () {
+  return gulp.src('src/result/**/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('htmlHint', function() {
+  return gulp.src('src/html/**/')
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter());
+});
+
+gulp.task('jslint', function() {
+  return gulp.src('src/js/**/')
+    .pipe(jslint())
+    .pipe(jslint.reporter());
+});
+
+gulp.task('clone', function (cb) {
+ return gulp.src(['src/img/**/', 'src/js/**/'])
+   .pipe(clone())
+ .pipe(gulp.dest('src/result/**/'));
+});
+
+//변경 scss
+gulp.task('sassLint', function() {
+  return gulp.src('src/scss/**/')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
 gulp.task('sass', function () {
-  return gulp.src('src/module/**/*.scss')
+  return gulp.src('src/scss/**/')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('src/css'));
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('src/scss/maps'))
+    .pipe(gulp.dest('src/result/css'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('src/module/**/*.scss', ['sass']);
-	gulp.watch('src/markup/*.html', 'src/module/**/*.html', ['fileinclude']);
+    gulp.watch('src/html/**/*.html', ['htmlHint']);
+    gulp.watch('src/scss/**/*.scss', ['sassLint']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch(['src/html/**/*.html','!src/html/include/**'] , ['fileinclude']);
+    gulp.watch(['src/img/**/', 'src/js/**/'] , ['clone']);
 });
 
 gulp.task('fileinclude', function() {
-  gulp.src(['src/markup/*.html','src/module/**/*.html'])
+  gulp.src(['src/html/**/*.html','!src/html/include/**'])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest('src/result'));
+    .pipe(gulp.dest('src/result/html'));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        files: ["src/module/**/*.*","src/markup/*.*"],
-        proxy: "http://localhost:80/dev/wordpress_project",
-        open: 'external',
-        logprefix: "bs"
-    });
-});
-
-gulp.task('default', ['watch','fileinclude','browser-sync']);
+gulp.task('default', ['clean', 'sass', 'fileinclude', 'clone', 'watch']);
